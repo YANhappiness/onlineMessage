72c33a7badf8928ac76b66deeb3ec985042268d2
yanhappiness <457029997@qq.com>
2017-06-06 13:00:41 +0800

    最新版本

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ var express = require("express");
 var app = express();
 var ejs = require("ejs");
 var router = require("./controller/router.js");
+//设置模版
 app.set("view engine","ejs");
-
+//移入静态文件
 app.use(express.static("./public"));
 
-app.get("/",router.showIndex);
-app.post("/",router.getData);
-app.use("/page",router.page);
-app.get("/del",router.del);
+app.get("/",router.showIndex); //首页
+app.post("/",router.getData);  //表单提交
+app.use("/page",router.page);  //分页
+app.get("/del",router.del);    //删除操作
 
 app.listen(3000);
--- controller/router.js
+++ controller/router.js
@@ -1,7 +1,6 @@
 
 var mongo = require("../modle/mongo.js");
 var formidable = require("formidable");
-
 var sd = require("silly-datetime");
 
 exports.showIndex = function(req,res){
@@ -9,9 +8,9 @@ exports.showIndex = function(req,res){
     mongo.getAllCount("message",function(count){
         //查询数据
          size = 3;
-         page = 1;
+         page = 0;
         //页数
-        num = Math.ceil(count/5);
+        num = Math.ceil(count/size);
         //分页 pageNum未获取到
         pageNum =  (req.url).split("/")[1];
         mongo.find("message",{},{
@@ -21,69 +20,84 @@ exports.showIndex = function(req,res){
         },function(err,docs){
             res.render("index",{"data":docs ,"num":num});
         });
-        return;
     })
 }
 
 //插入数据
-exports.getData = function(req,res){
+exports.getData = function(req,res,next){
     //获取时间
     var date = sd.format(new Date(), 'YYYYMMDDHHmmss');
     //获取数据
     var form = new formidable.IncomingForm();
+    //生成随机数
+    rand = parseInt(Math.random()*1000);
     form.parse(req, function(err, fields, files) {
         if(err){
             throw err;
         }
-        //插入数据
-        mongo.insertOne("message",{
-            "name":fields.name,
-            "message":fields.message,
-            "time":date
-        },function(err,docs){
-            res.redirect("/")
-        })
+
+        //判断输入值是否为空 为空不提交
+        if(fields.name !== '' && fields.message !== ''){
+            //插入数据
+            mongo.insertOne("message",{
+                "name":fields.name,
+                "message":fields.message,
+                "time":date,
+                "rand":date+rand
+            },function(err,docs){
+                res.redirect("/");
+            });
+        }
+        res.redirect("/"); //地址跳转
     });
-    return;
+    return next;
 }
 
 // 删除数据
-exports.del = function(req,res){
-    var time = req.query.time;
-    mongo.deleteMany("message",{"time":time},function(err,docs){
-        res.redirect("/");
-        console.log(time);
-    })
+exports.del = function(req,res,next){
+    var rand = req.query.rand;   //删除时间戳
+    mongo.deleteMany("message",{"rand":rand},function(err,docs){
+        res.render("/");
+
+        //res.redirect("back");     //删除数据后停留在本页 如果本页灭有数据则重定向到上一页
+        //if(){
+        //}
+    });
 }
 
 //分页
-exports.page = function(req,res){
-
+exports.page = function(req,res,next){
         //获取当前记录数
     mongo.getAllCount("message",function(count){
         //查询数据
-         size = 4;
+         size = 3;
          page = (req.url).split("/")[1];
+        //判断是否存在下一页
+        if(page == "next"){
+            console.log(page);
+        }
+
+
+
+        //判断是否存在上一页
+        if(page == "prev"){
+            if(page-1){
+                page = page-1;
+            }
+            page = page;
+            return page;
+        }
         //页数
-        num = Math.ceil(count/5);
-        //分页 
-        // pageNum =  (req.url).split("/")[1];
+        num = Math.ceil(count/size);
+        //分页
         mongo.find("message",{},{
             sort:{"time":-1},
             size:size,
             page:page
         },function(err,docs){
-            res.render("hello");
-            res.redirect("/");
+            res.render("index",{"data":docs ,"num":num});
         });
-        return;
+        return next;
     })
-
     //Error:Can`t set headers after they are sent
 }
-
-
-
-
-
-
